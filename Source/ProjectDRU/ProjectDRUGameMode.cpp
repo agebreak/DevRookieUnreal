c5e8f2d0a70a4b4d380d5ff292b580ecd3a57d74
박민근 <agebreak@naver.com>
2019-03-02 17:00:14 +0900

    -메인 캐릭터를 신비 캐릭터로 변경 / 애니메이션 스타터팩 애셋 추가

--- Source/ProjectDRU/ProjectDRUGameMode.cpp
+++ Source/ProjectDRU/ProjectDRUGameMode.cpp
@@ -3,15 +3,18 @@
 #include "ProjectDRUGameMode.h"
 #include "ProjectDRUPlayerController.h"
 #include "ProjectDRUCharacter.h"
+#include "MyPlayerController.h"
+#include "MyCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AProjectDRUGameMode::AProjectDRUGameMode()
 {
-	// use our custom PlayerController class
-	PlayerControllerClass = AProjectDRUPlayerController::StaticClass();
+	// use our custom PlayerController class	
+	PlayerControllerClass = AMyPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT(
+	"/Game/Blueprint/MyCharacter_BP.MyCharacter_BP_C"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
